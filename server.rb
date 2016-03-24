19d23c374d2c5340b8f940c93573614e61091589
Molly Kraus <mollyjkraus@gmail.com>
2016-03-24 11:26:03 -0400

    push

---
+++ server.rb
@@ -0,0 +1,34 @@
+# db is portfolio
+module Portfolio
+  class Server < Sinatra::Base
+    
+    get "/" do
+      erb :index
+    end
+
+    get "/project/1" do
+      erb :simon   
+    end 
+
+    get "/project/2" do
+      erb :fringely   
+    end
+
+    get "/project/3" do
+      erb :midways    
+    end
+
+    get "/project/4" do
+      erb :partylines    
+    end
+
+    get "/contact" do
+    	erb :contact
+    end
+
+    get "/about"do
+        erb :about
+    end
+
+  end
+end
